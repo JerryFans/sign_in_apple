09028124c879e444d45f949a5df8da535d424e87
jerryfans <>
2021-09-21 17:31:07 +0800

    remove import

--- test/sign_in_apple_test.dart
+++ test/sign_in_apple_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sign_in_apple/sign_in_apple.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('sign_in_apple');
